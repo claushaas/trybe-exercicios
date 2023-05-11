7d4e04015838bc0daff55cb0532778dba3784463
Claus <claus.haas@me.com>
2023-05-11 19:06:17 -0300

    adiciona exercício removendo elementos

---
+++ fundamentos/secao-4-dom-eventos-webstorage/dia-2-trabalhando-com-elementos/removendo elementos/script.js
@@ -0,0 +1,5 @@
+// Remova a tag h2, filha do elemento where-are-you.
+
+const h2 = document.querySelector('#where-are-you h2')
+const whereAreYou = document.getElementById('where-are-you')
+whereAreYou.removeChild(h2)
