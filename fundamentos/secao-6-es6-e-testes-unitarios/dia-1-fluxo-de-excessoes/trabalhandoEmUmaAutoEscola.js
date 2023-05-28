9c8cdaa2b3b7c3f26e70f4b6ed44bd1713c107c9
Claus <claus.haas@me.com>
2023-05-28 16:59:49 -0300

    adiciona exercícios

---
+++ fundamentos/secao-6-es6-e-testes-unitarios/dia-1-fluxo-de-excessoes/trabalhandoEmUmaAutoEscola.js
@@ -0,0 +1,9 @@
+const studentRegister = (name, age) => {
+  try {
+    if (!name || !age) throw new Error('Todas as informações são necessárias')
+    if (age < 18) throw new Error('Ops, infelizmente nesse momento você não pode fazer as aulas')
+    return `${name}, seja bem-vindo(a) à AuTrybe!`
+  } catch (error) {
+    throw error.message
+  }
+}
