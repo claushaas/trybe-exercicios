00f1cd132c8432c69a320d7f13ef7bbde241ffc1
Claus Haas <claus.haas@me.com>
2023-06-09 09:27:38 -0300

    adiciona exercícios

---
+++ fundamentos/secao-7-hofs/dia-4-spread-rest-operators/exercicio2.js
@@ -0,0 +1,7 @@
+// Crie uma função sum que, dado um número ilimitado de parâmetros, retorna a soma desses parâmetros. Ao chamar a função dessa forma : sum(4,5,6), o número 15 deve ser retornado.
+
+const sum = (...args) => {
+  return args.reduce((total, number) => total + number, 0)
+}
+
+console.log(sum(4, 5, 6, 15))
